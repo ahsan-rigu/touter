16affeee79d7ed9880f0d3a365c24e1e9909083e
ahsan-rigu <114474110+ahsan-rigu@users.noreply.github.com>
2023-06-20 15:55:50 +0530

    feaT: added basic files feat: Added router and contexts to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import AuthContextProvider from "./contexts/AuthContext";
+import UserContextProvider from "./contexts/UserContext";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <UserContextProvider>
+          <App />
+        </UserContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
